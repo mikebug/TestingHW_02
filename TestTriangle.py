a50563690242c4662e5e9178a30955f3108a6a88
mikebug <mbuglion@stevens.edu>
2018-09-08 23:14:17 -0400

    Part 1
    added two new tests that test the errors in the project:
    testvalidTriangle is the imputs able to make a triangle
    
    testTriangle tests if the values are valid

--- TestTriangle.py
+++ TestTriangle.py
@@ -20,13 +20,18 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleA(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
+
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testTriangle(self):
+        self.assertNotEqual(classifyTriangle(1,1,1),'InvalidInput',"111 should not receive a InvalidInput")
 
+    def testvalidTriangle(self):
+        self.assertEqual(classifyTriangle(50,3,4),'NotATriangle','This is not a triangle')
 
 
 if __name__ == '__main__':
